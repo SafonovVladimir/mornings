da89199a6622a0bf844d83b92f3a3c6050012b91
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-06-02 11:24:23 +0300

    add 01 of june

--- 05 may/30.py
+++ 05 may/30.py
@@ -1,6 +1,14 @@
 def minSubsequence(nums: list[int]) -> list[int]:
-    ...
+    max_num = max(nums)
+    max_num_list = [max_num]
+    nums.remove(max_num)
+    while sum(max_num_list) <= sum(nums):
+        max_num_list.append(max(nums))
+        nums.remove(max(nums))
+
+    return max_num_list
 
 
-nums = [4,3,10,9,8]
+# nums = [4, 3, 10, 9, 8]
+nums = [4, 4, 7, 6, 7]
 print(minSubsequence(nums))
